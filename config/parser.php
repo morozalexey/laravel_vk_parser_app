cfc9f83dcba66b830137017e45eec3cd5a3de57f
Alexey Moroz <15webmoroz@gmail.com>
2023-04-10 13:11:46 +0300

    parser config

---
+++ config/parser.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'vk_token' => env('VK_TOKEN', ''),
+    'vk_group_id' => env('VK_GROUP_ID', ''),
+    'tg_token' => env('TG_TOKEN', ''),
+    'tg_chat_id' => env('TG_CHAT_ID', ''),
+];
